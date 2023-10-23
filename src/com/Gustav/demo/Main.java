1ee0645afb02af9432f88d3d6b559d7665e4cd33
GustavParment <gustav.parment1993@gmail.com>
2023-10-23 13:57:13 +0200

    final commit

--- src/com/Gustav/demo/Main.java
+++ src/com/Gustav/demo/Main.java
@@ -1,10 +1,38 @@
 package com.Gustav.demo;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        List<String> stringList = new ArrayList<>();
+        boolean exit = false;
+
+        do {
+
+            System.out.print("Add letters or 'quit' to exit: ");
+            String input = sc.nextLine();
+
+            switch (input) {
+
+                case "quit" -> exit = true;
+                default -> stringList.add(input);
+
+            }
+
+        }while(!exit);
+        String listToStr = String.join("", stringList);
+        System.out.println(listToStr);
+
+
+
+
+
 
     }
 }
