e1c02bbfca9c4228cad9ad042511813ac55ecc4b
GustavParment <gustav.parment1993@gmail.com>
2023-10-23 13:02:00 +0200

    first commit

--- src/com/Gustav/demo/Main.java
+++ src/com/Gustav/demo/Main.java
@@ -6,45 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int weekday;
-
-        findWeekday();
-
-
-
-    }
-
-    public static void findWeekday(){
-
-        Scanner sc = new Scanner(System.in);
-        boolean exit = true;
-
-        do {
-
-            System.out.println("Username? , type '8' to exit : ");
-
-            switch (sc.nextInt()) {
-                case 1 -> System.out.println("I'ts Monday");
-                case 2 -> System.out.println("It's Tuesday");
-                case 3 -> System.out.println("It's Wednesday ");
-                case 4 -> System.out.println("It's Thursday");
-                case 5 -> System.out.println("It's Friday");
-                case 6 -> System.out.println("It's Saturday ");
-                case 7 -> System.out.println("It's Söndag");
-                default -> System.out.println("That's not an correct input");
-                case 9 -> {
-                    exit = false;
-                    sayGoodbye("Arnold");
-                }
-
-            }
-        }while (exit);
-
-    }
-    public static void sayGoodbye(String username){
-
-        System.out.println("Goodbye " + username);
-
-
     }
 }
+
+
+
